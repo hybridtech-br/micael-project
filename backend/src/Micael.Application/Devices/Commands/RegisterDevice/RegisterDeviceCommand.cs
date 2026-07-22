65bbb87d70dcac464f7bd5b05ed5a943a4fb571b
Hybrid Tecnologia Inteligente <contato@hybridtech.com.br>
2026-07-22 12:53:35 -0300

    feat(application): adicionar comando de registro de dispositivo

---
+++ backend/src/Micael.Application/Devices/Commands/RegisterDevice/RegisterDeviceCommand.cs
@@ -0,0 +1,11 @@
+using Micael.Domain.Entities;
+
+namespace Micael.Application.Devices.Commands.RegisterDevice;
+
+public sealed record RegisterDeviceCommand(
+    Guid TenantId,
+    string Name,
+    string IpAddress,
+    int Port,
+    DeviceType Type,
+    DeviceProtocol Protocol);
