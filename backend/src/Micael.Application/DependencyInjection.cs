167ee9bea2cc7860cedb5f64375beb27a8565d22
Hybrid Tecnologia Inteligente <contato@hybridtech.com.br>
2026-07-22 15:38:08 -0300

    feat(application): registrar MediatR validadores e pipeline de validacao

---
+++ backend/src/Micael.Application/DependencyInjection.cs
@@ -0,0 +1,25 @@
+using FluentValidation;
+using MediatR;
+using Micael.Application.Common.Behaviors;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Micael.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        var assembly = typeof(DependencyInjection).Assembly;
+
+        services.AddMediatR(configuration =>
+            configuration.RegisterServicesFromAssembly(assembly));
+
+        services.AddValidatorsFromAssembly(assembly);
+
+        services.AddTransient(
+            typeof(IPipelineBehavior<,>),
+            typeof(ValidationBehavior<,>));
+
+        return services;
+    }
+}
